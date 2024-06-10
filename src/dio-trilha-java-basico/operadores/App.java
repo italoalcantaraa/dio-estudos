92b4e73b0e7535209594698930f1c71663669c8b
italoalcantaraa <italoalcantara6@gmail.com>
2024-06-10 20:58:57 -0300

    operadores Unários

---
+++ src/dio-trilha-java-basico/operadores/App.java
@@ -0,0 +1,14 @@
+package operadores;
+
+public class App {
+    public static void main(String[] args) {
+
+        String name = " ";
+
+        name = 1 + 1 + " Italo";
+        System.out.println(name);
+
+        name = "Italo " + 1 + 1;
+        System.out.println(name);
+    }
+}
---
+++ src/dio-trilha-java-basico/operadores/operadores3.java
@@ -0,0 +1,14 @@
+package operadores;
+
+public class operadores3 {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println(numero);
+        
+        numero = -5;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+}
