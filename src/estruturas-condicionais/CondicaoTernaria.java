afe1c03130a4cc383d9989b542926aac59973396
italoalcantaraa <italoalcantara6@gmail.com>
2024-06-12 06:33:39 -0300

    Condicional ternária

---
+++ src/estruturas-condicionais/CondicaoTernaria.java
@@ -0,0 +1,9 @@
+public class CondicaoTernaria {
+    public static void main(String[] args) {
+        int mediaAluno = 10;
+        String resultadoAluno = mediaAluno >= 7 ? "Aprovado"
+                : mediaAluno < 7 && mediaAluno >= 5 ? "Recuperação!" : "Reprovado";
+
+                System.out.println("Estado: " + resultadoAluno);
+    }
+}
